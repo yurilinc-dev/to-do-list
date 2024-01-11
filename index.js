5d482617276a3ccdb84b8528bd7d6f515f9bf1a8
yurilinc-dev <yurilinc.tech@gmail.com>
2024-01-11 10:50:49 -0300

    Endpoint creation between index, ejs, script and style

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/home', (req, res) => {
-  res.send('Hello World');
+  res.render("index");
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
