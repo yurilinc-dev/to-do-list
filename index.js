e1e4a4b19ec2cf0afdf091b8bcc0c975c23a3d59
yurilinc-dev <yurilinc.tech@gmail.com>
2024-01-11 18:25:32 -0300

    config index, routes and controller

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes/routes")
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")));
-
-app.get('/home', (req, res) => {
-  res.render("index");
-});
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
