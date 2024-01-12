731cae425aa6fa68617032b4b220c63b4c0e924b
yurilinc-dev <yurilincoln.tech@gmail.com>
2024-01-12 00:40:37 -0300

    config mongoose, tasks and html/ejs

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const routes = require("express").Router();
-const TaskController = require("../controller/TaskController")
+const TaskController = require("../controller/TaskController");
 
-routes.get("/home", TaskController.getAll)
+routes.get("/", TaskController.getAllTasks);
+routes.post("/create", TaskController.createTask);
 
 module.exports = routes;
