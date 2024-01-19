85891bcc3d7a0806b39d32e809a39bb2d077194c
yurilinc-dev <yurilincoln.tech@gmail.com>
2024-01-18 22:33:40 -0300

    check task true ? false

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -80,12 +80,26 @@ const deleteOneTask = async (req, res) => {
         res.status(500).send({ error: err.message });
     }
 
-}
+};
+
+const taskCheck = async (req, res) => {
+    try {
+        const task = await Task.findOne({ _id: req.params.id });
+
+        task.check ? task.check = false : task.check = true
+
+        await Task.updateOne({ _id: req.params.id }, task)
+        res.redirect("/")
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
 
 module.exports = {
     getAllTasks,
     createTask,
     getById,
     updateOneTask,
-    deleteOneTask
+    deleteOneTask,
+    taskCheck
 };
